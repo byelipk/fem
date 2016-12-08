7b4505eca83a9e3a1208968ad936e93e8a6e46b7
byelipk <byelipk@gmail.com>
2016-12-08 13:36:41 -0500

    Complete exercise 3

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,23 +2,35 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+// Broccoli stuff
+var Filter    = require('broccoli-filter');
+var log       = require('broccoli-stew').log;
+var debugTree = require('broccoli-stew').debug;
+
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
         // Add options here
     });
 
-    // Use `app.import` to add additional libraries to the generated
-    // output files.
-    //
-    // If you need to use different assets in different
-    // environments, specify an object as the first parameter. That
-    // object's keys should be the environment name and the values
-    // should be the asset to use in that environment.
-    //
-    // If the library that you are including contains AMD or ES6
-    // modules that you would like to import into your application
-    // please specify an object with the list of modules as keys
-    // along with the exports of each module as its value.
+    function MyFilter(inputNode) {
+      Filter.call(this, inputNode);
+    }
 
-    return app.toTree();
+    MyFilter.prototype = Object.create(Filter.prototype);
+
+    // How are we going to process the string?
+    MyFilter.prototype.processString = function stringProcessor(existingString) {
+      let prepend = `
+// vendor.js
+//
+// (c) 2016 😺😸😹😻😼😽🙀😿 All Rights Reserved
+// generated at: ${new Date()}
+//\n\n`
+      return prepend + existingString;
+    };
+
+    MyFilter.prototype.extensions = ["js"];      // only target js files
+    MyFilter.prototype.targetExtension = ["js"]; // emit a js file
+
+    return new MyFilter(app.toTree());
 };

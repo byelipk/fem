a95b60e1dd9fa925376a2fa1e46211793980f596
byelipk <byelipk@gmail.com>
2016-12-08 12:17:25 -0500

    Complete exercise 2

---
+++ app/initializers/geolocation.js
@@ -0,0 +1,31 @@
+// Exercise 2
+// Use the registration API to pause the boot of your app till
+// you download your geo coordinates. Once you have the coordinates
+// allow your app to continue loading.
+//
+// Make the data available on the container.
+export function initialize(application) {
+  const geo = navigator.geolocation;
+
+  application.deferReadiness();
+
+  geo.getCurrentPosition(function callback(pos) {
+    let loc = {
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude
+    };
+
+    console.log(loc);
+
+    application.register('data:location', loc, {
+      instantiate: false
+    });
+
+    application.advanceReadiness();
+  });
+}
+
+export default {
+  name: 'geolocation',
+  initialize
+};
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 import { default as math, PI } from 'math';
 
-const { Route, inject } = Ember;
+const { Route, inject, getOwner } = Ember;
 
 export default Route.extend(ApplicationRouteMixin, {
+
+    session: inject.service(),
 
     beforeModel() {
       this._super(...arguments);
@@ -13,7 +15,14 @@ export default Route.extend(ApplicationRouteMixin, {
       console.log(PI);
     },
 
-    session: inject.service(),
+    setupController(controller) {
+      this._super(...arguments);
+
+      controller.set('loc',
+        getOwner(this).lookup('data:location'));
+    },
+
+
     actions: {
         logout() {
             this.get('session').invalidate();
